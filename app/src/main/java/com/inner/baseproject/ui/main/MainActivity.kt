ccd7c6c6ea7c0814aa73aa0ffecb2e90f1f80c54
inner <khsbs001@gmail.com>
2021-09-23 01:34:14 +0900

    Compose tutorial with Step 4

--- app/src/main/java/com/inner/baseproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/inner/baseproject/ui/main/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.inner.baseproject.ui.main
 
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.inner.baseproject.R
 import com.inner.baseproject.base.BaseActivity
 import com.inner.baseproject.databinding.ActivityMainBinding
@@ -14,5 +31,65 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            PreviewMessageCard()
+        }
     }
+
+    @Composable
+    fun MessageCard(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        name = "Dark Mode",
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true
+    )
+    @Composable
+    fun PreviewMessageCard() {
+        // MessageCard("Android")
+        MessageCard(
+            msg = Message("Colleague", "Hey, take a look at Jetpack Compose, is it great? idk")
+        )
+    }
+
+    data class Message(val author: String, val body: String)
+
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = msg.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.background(Color.Blue).padding(4.dp),
+                        style = MaterialTheme.typography.body2
+                    )
+                }
+            }
+        }
+    }
+
+
 }
